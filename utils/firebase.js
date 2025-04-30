0fa1eaf8986bf78a06feff3faefdaaec16ce074b
shubhmodi07 <shubhmodi07@gmail.com>
2025-04-30 14:57:01 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-p-qzaWY-T9dIIAGAohU_Vv9F3OjUvfM",
-  authDomain: "zense-blogs.firebaseapp.com",
-  projectId: "zense-blogs",
-  storageBucket: "zense-blogs.firebasestorage.app",
-  messagingSenderId: "50221076458",
-  appId: "1:50221076458:web:3aaa6bef36fba2966aec93",
-  measurementId: "G-DGCBJ54FTV"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
